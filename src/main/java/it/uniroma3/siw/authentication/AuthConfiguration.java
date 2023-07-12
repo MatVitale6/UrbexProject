734ef7e80e12d5f3b8e87443e9928b8026666859
MatVitale6 <mat.vitale6@stud.uniroma3.it>
2023-07-12 15:36:44 +0200

    da verificare se mappa visibile anche per chi non é registrato

--- src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -52,7 +52,7 @@ public class AuthConfiguration {
         httpSecurity
         .csrf().and().cors().disable()
         .authorizeHttpRequests()
-        .requestMatchers(HttpMethod.GET, "/", "/index", "/login", "/register", "/css/**", "/images/**","/formSearchPlaces","favicon.ico", "/place/**", "/places", "/place").permitAll()
+        .requestMatchers(HttpMethod.GET, "/", "/index", "/login", "/register", "/css/**", "/images/**","/js/**","/formSearchPlaces","favicon.ico", "/place/**", "/places", "/place").permitAll()
         .requestMatchers(HttpMethod.POST, "/login", "/register", "/place/**", "/searchPlacesByRegion","/searchPlacesByAddress").permitAll()
         .requestMatchers(HttpMethod.GET, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
         .requestMatchers(HttpMethod.POST, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
